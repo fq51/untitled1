7136c046fdd059e17ac75750dc4407a1ec49940f
0ReC0 <kitanin.ser@mail.ru>
2024-02-03 09:26:18 +0300

    Slide changing added

---
+++ src/Frame1.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Frame1 extends JFrame {
+    private JButton button1;
+    private JPanel panel;
+
+    public Frame1(){
+        setTitle("1");
+        setContentPane(panel);
+        setBounds(100,100,200,200);
+        setVisible(true);
+
+        button1.addActionListener((event)->{
+            new Frame2();
+            dispose();
+        });
+
+    }
+}
---
+++ src/Frame2.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.jar.JarEntry;
+
+public class Frame2 extends JFrame {
+    private JPanel panel;
+
+    public Frame2(){
+        setTitle("2");
+        setContentPane(panel);
+        setBounds(100,100,200,200);
+        setVisible(true);
+
+    }
+}
